548231a73600ac578e1400a7d475c5d56effff2e
nathan-mccraw <naterltw175@gmail.com>
2021-10-08 15:58:11 -0400

    Add class names to html dynamic html elements. Add formatDate function.

--- script.js
+++ script.js
@@ -4,30 +4,60 @@ function getTranscript() {
       if (!response.ok) {
         throw new Error(console.log(`Error: ${response.status}`));
       }
+
       const res = await response.json();
+
       const documentDate = document.getElementById("documentDate");
-      documentDate.textContent = res.data.conversationDate;
+      documentDate.textContent = formatDate(res.data.conversationDate);
+
       const chatWindow = document.getElementById("chatWindow");
+      const firstUser = res.data.messages[0].username;
       for (let message of res.data.messages) {
         const messageContainer = document.createElement("div");
         messageContainer.setAttribute("class", "messageContainer");
+
         const userImg = document.createElement("img");
         userImg.src = message.image;
-        const messageBody = document.createElement("div");
-        const messageText = document.createElement("p");
-        messageText.textContent = message.message;
-        const username = document.createElement("p");
-        username.textContent = message.username;
-        const timestamp = document.createElement("p");
-        timestamp.textContent = message.timestamp;
-        messageBody.append(messageText, username, timestamp);
-        messageContainer.append(userImg, messageBody);
+        userImg.setAttribute("class", "userImage");
+
+        const messageTextElement = document.createElement("p");
+        messageTextElement.setAttribute("class", "messageText");
+        messageTextElement.textContent = message.message;
+
+        const usernameElement = document.createElement("p");
+        const userClass =
+          message.username === firstUser ? "firstUser" : "secondUser";
+        usernameElement.setAttribute("class", userClass);
+        usernameElement.textContent = message.username;
+
+        const timestampElement = document.createElement("p");
+        timestampElement.setAttribute("class", "time");
+        timestampElement.textContent = message.timestamp;
+
+        const messageBodyElement = document.createElement("div");
+        messageBodyElement.append(
+          messageTextElement,
+          usernameElement,
+          timestampElement
+        );
+        messageContainer.append(userImg, messageBodyElement);
         chatWindow.append(messageContainer);
       }
     })
     .catch((error) => {
       console.log(error);
     });
+}
+
+function formatDate(conversationDate) {
+  let date = new Date(conversationDate);
+  let localeStringOptions = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  };
+  return date.toLocaleString("en-US", localeStringOptions);
 }
 
 getTranscript();
