7388e31cfa8925a8273af0816ff284ec48a124e6
nathan-mccraw <naterltw175@gmail.com>
2021-10-08 16:38:44 -0400

    Add createTranscript, formatTime, and createPElement function. Refactor dynamic addition to className.  Add focused to class list on message containers.

--- script.js
+++ script.js
@@ -4,49 +4,54 @@ function getTranscript() {
       if (!response.ok) {
         throw new Error(console.log(`Error: ${response.status}`));
       }
-
       const res = await response.json();
-
-      const documentDate = document.getElementById("documentDate");
-      documentDate.textContent = formatDate(res.data.conversationDate);
-
-      const chatWindow = document.getElementById("chatWindow");
-      const firstUser = res.data.messages[0].username;
-      for (let message of res.data.messages) {
-        const messageContainer = document.createElement("div");
-        messageContainer.setAttribute("class", "messageContainer");
-
-        const userImg = document.createElement("img");
-        userImg.src = message.image;
-        userImg.setAttribute("class", "userImage");
-
-        const messageTextElement = document.createElement("p");
-        messageTextElement.setAttribute("class", "messageText");
-        messageTextElement.textContent = message.message;
-
-        const usernameElement = document.createElement("p");
-        const userClass =
-          message.username === firstUser ? "firstUser" : "secondUser";
-        usernameElement.setAttribute("class", userClass);
-        usernameElement.textContent = message.username;
-
-        const timestampElement = document.createElement("p");
-        timestampElement.setAttribute("class", "time");
-        timestampElement.textContent = message.timestamp;
-
-        const messageBodyElement = document.createElement("div");
-        messageBodyElement.append(
-          messageTextElement,
-          usernameElement,
-          timestampElement
-        );
-        messageContainer.append(userImg, messageBodyElement);
-        chatWindow.append(messageContainer);
-      }
+      createTranscript(res.data);
     })
     .catch((error) => {
       console.log(error);
     });
+}
+
+function createTranscript(data) {
+  const documentDate = document.getElementById("documentDate");
+  documentDate.textContent = formatDate(data.conversationDate);
+
+  const chatWindow = document.getElementById("chatWindow");
+  const firstUser = data.messages[0].username;
+
+  for (let message of data.messages) {
+    const messageContainer = document.createElement("div");
+    messageContainer.className = "messageContainer";
+    if (message.focused) messageContainer.classList.add("focused");
+
+    const userImg = document.createElement("img");
+    userImg.src = message.image;
+    userImg.className = "userImage";
+
+    const messageTextElement = createPElement("messageText", message.message);
+
+    const userClass =
+      message.username === firstUser ? "firstUser" : "secondUser";
+    const usernameElement = createPElement(userClass, message.username);
+
+    const timestampElement = createPElement(
+      "time",
+      formatTime(message.timestamp)
+    );
+
+    const messageBodyElement = document.createElement("div");
+    messageBodyElement.append(
+      messageTextElement,
+      usernameElement,
+      timestampElement
+    );
+
+    message.username === firstUser
+      ? messageContainer.append(userImg, messageBodyElement)
+      : messageContainer.append(messageBodyElement, userImg);
+
+    chatWindow.append(messageContainer);
+  }
 }
 
 function formatDate(conversationDate) {
@@ -58,6 +63,19 @@ function formatDate(conversationDate) {
     year: "numeric",
   };
   return date.toLocaleString("en-US", localeStringOptions);
+}
+
+function formatTime(timestamp) {
+  return new Date(timestamp).toLocaleTimeString("en-US", {
+    timeStyle: "short",
+  });
+}
+
+function createPElement(className, content) {
+  PElement = document.createElement("p");
+  PElement.className = `${className}`;
+  PElement.textContent = `${content}`;
+  return PElement;
 }
 
 getTranscript();
